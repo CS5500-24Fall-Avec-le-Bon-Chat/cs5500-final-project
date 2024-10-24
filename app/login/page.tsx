08cbad3ae405e7fd8d6b808325c804389c402ce2
Steven Hu <steven.h.me@outlook.com>
2024-10-23 18:27:07 -0700

    feat: add project structure and login page as a template

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center mt-64 mx-auto w-3/4 max-w-2xl flex-col gap-5">
+      <h2>Login Page</h2>
+      <Input type="text" placeholder="Username" />
+      <Button className="w-20 self-center">Login</Button>
+    </div>
+  );
+}
