4e20a108634014d09e7b4ef09b07a54a6063897f
Steven Hu <steven.h.me@outlook.com>
2024-11-15 09:36:09 -0800

    feat: add redirect on home and login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,14 +2,19 @@
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import {useRouter} from "next/navigation";
 
 export default function LoginPage() {
+  const router = useRouter();
+  const handleLogin = () => {
+    router.push("/fundraiser");
+  }
+
   return (
     <div className="flex justify-center mt-64 mx-auto w-3/4 max-w-2xl flex-col gap-5">
       <h2>Login Page</h2>
       <Input type="text" placeholder="Username" />
-      <Button className="w-20 self-center">Login</Button>
-      <p>After login, navigate to the personal page of the Fundraiser / Coordinator? -- Cathy</p>
+      <Button className="w-20 self-center" onClick={handleLogin}>Login</Button>
     </div>
   );
 }
