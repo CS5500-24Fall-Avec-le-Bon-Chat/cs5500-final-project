4cc98df8a5e71f4b8d3014ed1ccf4cf4680fc08a
cathyfu1215 <phoenixfucong@gmail.com>
2024-11-26 15:26:08 -0800

    added picture to the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,18 +4,19 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import './styles.css'; 
 
 export default function LoginPage() {
   const router = useRouter();
   const [username, setUsername] = useState("");
   const handleLogin = () => {
-    if (!username) return
+    if (!username) return;
     localStorage.setItem("username", username);
     router.push("/fundraiser");
   };
 
   return (
-    <div className="flex justify-center mt-64 mx-auto w-3/4 max-w-2xl flex-col gap-5">
+    <div className="flex justify-center mt-8 mx-auto w-3/4 max-w-2xl flex-col gap-5 background">
       <h2 className="self-center mb-4">Fundraisers Login Portal</h2>
       <Input
         type="text"
